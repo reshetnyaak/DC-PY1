5b09245a9b07deebf2ce2bc28bbb40858e758637
reshetnyaak <115507045+reshetnyaak@users.noreply.github.com>
2022-10-16 18:58:53 +0300

    Старожук А.А

---
+++ Лабораторная 3/task_1.py
@@ -0,0 +1,8 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+
+result = True or False and False # TODO подставить результат выражения
+result = True or False
+result = True
+print(src == result)
